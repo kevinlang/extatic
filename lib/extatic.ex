b537ad9623d2fd8bf4c05a759667c6bfc0ec321d
Kevin Lang <me@gnalck.com>
2021-08-06 17:06:20 -0400

    init commit

---
+++ lib/extatic.ex
@@ -0,0 +1,18 @@
+defmodule Extatic do
+  @moduledoc """
+  Documentation for `Extatic`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Extatic.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
